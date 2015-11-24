2b7865ab943b2a51c06019602c8b65bc9d574111
ZooBot Mars <sasa345@yahoo.com>
2015-11-23 20:46:02 -0700

    total calculation added

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,8 +5,13 @@ class ShopsController < ApplicationController
   end
 
   def show
+    @total = 0
     @items = Shop.find(params[:id]).items
+    @items.each do |item|
+      @total += item.price
+    end
     @total = 2.5
+    binding.pry
   end
 
 end
